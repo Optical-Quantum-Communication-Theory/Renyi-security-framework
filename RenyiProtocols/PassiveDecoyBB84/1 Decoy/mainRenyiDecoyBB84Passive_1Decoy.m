7162e4e894c90b7608552985bb147695cc1d287f
LKamin96 <lkamin@uwaterloo.ca>
2025-05-24 19:13:40 -0400

    Created new main file and renamed preset
    new preset name includes number of decoys

---
+++ RenyiProtocols/PassiveDecoyBB84/1 Decoy/mainRenyiDecoyBB84Passive_1Decoy.m
@@ -0,0 +1,76 @@
+%pick preset
+clear all
+qkdInput = RenyiDecoyBB84PassivePreset_1Decoy();
+
+%List of mutiple total signals sent
+N_list = [1e6,1e8,1e10];
+
+%Loss
+%total array of loss values to iterate over
+lossdB = linspace(0,40,21);
+transmittance = 10.^(-lossdB/10);
+
+%list of maximal element of loss ordered epsilon_int = 0, 1e-6, 1e-4, 1e-2
+%(named delta in description)
+lossList = [[7,16,21]; [6,16,21] ;[6,14,15]; [4,4,4]];
+
+% list of epsilon_int values
+epsilonInt_List = [0, 1e-6, 1e-4, 1e-2];
+
+%filestring for optimal values
+filestrOptVals = "optimalValues\optValsPassiveDecoyBB84_1decoy_N=";
+
+% store qkdInput for later
+qkdInputSave = qkdInput;
+
+for indexEps = 1:numel(epsilonInt_List)
+    for indexSignals = 1:numel(N_list)
+        %set transmittance list for current value of N
+        transmittanceTemp = transmittance(1:lossList(indexEps,indexSignals));
+    
+        %Load optimal values for current number of signal values
+        fileStrTemp = filestrOptVals + sprintf("%.2e",N_list(indexSignals)) ...
+            + sprintf("%.2e",epsilonInt_List(indexEps)) +".csv";
+        %optimal values are sorted in coulmns as | logRenyiAlpha| ...
+        optvals = readmatrix(fileStrTemp);
+    
+        for indexLoss = 7:numel(transmittanceTemp)
+            fprintf("Iteration %.0f of %.0f for N=%.0e with eps_int=%.2e",indexLoss,...
+                numel(transmittanceTemp),N_list(indexSignals),epsilonInt_List(indexEps))
+    
+            %Add total signals sent from list above
+            qkdInput.addFixedParameter("Ntot",N_list(indexSignals))
+        
+            %Add loss until element from list above
+            qkdInput.addScanParameter("transmittance", num2cell(transmittance(indexLoss)));  
+        
+            %Add Renyi param from optimal values
+            % fixed alpha
+            logAlpha = optvals(indexLoss,1);
+            qkdInput.addFixedParameter("logrenyiAlpha", logAlpha);
+            
+            % % optimize alpha
+            % logrenyiAlpha.lowerBound = -5;
+            % logrenyiAlpha.upperBound = -0.5;
+            % logrenyiAlpha.initVal = logAlpha;
+            % qkdInput.addOptimizeParameter("logrenyiAlpha", logrenyiAlpha);
+    
+            % run the QKDSolver with this input
+            results(indexLoss) = MainIteration(qkdInput);
+        end
+        %edit qkdinput to save correct results
+        qkdInput.addScanParameter("transmittance", num2cell(transmittanceTemp));
+    
+        %filestring for saving
+        filestr = sprintf("data/RenyiDecoyBB84PassiveResults_%.2e",N_list(indexSignals)) + ...
+            sprintf("_epsInt=%.2e",epsilonInt_List(indexEps)) + "_1decoy.mat";
+    
+        % save the results and preset to a file
+        %save results
+        results = results(1:numel(transmittanceTemp));
+        save(filestr,"results","qkdInput");
+    end
+end
+
+%% plot the last result
+QKDPlot.simple1DPlot(qkdInput,results,"xScaleStyle","dB","yScaleStyle","log")
