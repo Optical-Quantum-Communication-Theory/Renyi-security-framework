0679921f93fa11dbc35ff09732795e82e714ca60
LKamin96 <lkamin@uwaterloo.ca>
2025-09-17 17:59:23 -0400

    Update minmaxPoisVec.m
    Corrected the lower bound in calculating the minimum probability. Previous results are still valid, but a lower bound

--- RenyiProtocols/PassiveDecoyBB84/minmaxPoisVec.m
+++ RenyiProtocols/PassiveDecoyBB84/minmaxPoisVec.m
@@ -24,36 +24,33 @@ function [minProb,maxProb] = minmaxPois(mu,deltaMu,photonNum)
         photonNum (1,1) double {mustBeInteger}
     end
 
-    %Only positive intensities allowed
-    muMinAllowed = max(mu - deltaMu,0);
-
     if photonNum == 0
-        %For 0 photons min and max of Poisson dist on interval [mu-delta,mu+delta]
+        %For 0 photons min and max of Poisson dist on interval [mu,mu+delta]
         % are always the end points
         minProb = Poisson(mu+deltaMu,0);
-        maxProb = Poisson(muMinAllowed,0);
+        maxProb = Poisson(mu,0);
     else
-        %For photonNum =/= 0 min of Poisson dist on interval [mu-delta,mu+delta]
+        %For photonNum =/= 0 min of Poisson dist on interval [mu,mu+delta]
         % is achieved at either end points
-        minProb = min(Poisson(muMinAllowed,photonNum), ...
+        minProb = min(Poisson(mu,photonNum), ...
                       Poisson(mu + deltaMu,photonNum));
         
-        %For photonNum =/= 0 max of Poisson dist on interval [mu-delta,mu+delta]
+        %For photonNum =/= 0 max of Poisson dist on interval [mu,mu+delta]
         % is achieved at either end points or at mu = photonNum if
-        % photonNum in [mu-delta,mu+delta]
+        % photonNum in [mu,mu+delta]
 
-        %Check if photonNum in [mu-delta,mu+delta]
-        insideRange = discretize(photonNum,[mu - deltaMu,mu + deltaMu]) == 1;
+        %Check if photonNum in [mu,mu+delta]
+        insideRange = discretize(photonNum,[mu,mu + deltaMu]) == 1;
 
         if insideRange
-            %if photonNum in [mu-delta,mu+delta] 3 possible values for mu
-            maxProb = max([Poisson(muMinAllowed,photonNum), ...
+            %if photonNum in [mu,mu+delta] 3 possible values for mu
+            maxProb = max([Poisson(mu,photonNum), ...
                       Poisson(mu + deltaMu,photonNum), ...
                       Poisson(photonNum,photonNum)],[],'all');
         else
-            %if photonNum not in [mu-delta,mu+delta] only end points need
+            %if photonNum not in [mu,mu+delta] only end points need
             %to be considered
-            maxProb = max([Poisson(muMinAllowed,photonNum), ...
+            maxProb = max([Poisson(mu,photonNum), ...
                       Poisson(mu + deltaMu,photonNum)],[],'all');
         end
     end
